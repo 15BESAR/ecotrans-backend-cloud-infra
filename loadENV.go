e20fc17a264d6850765f386a9d96a254c6153923
rayleigh18 <sitanggangirfan271000@gmail.com>
2022-05-17 09:19:26 +0700

    Change the env variable to load env

--- loadENV.go
+++ loadENV.go
@@ -7,15 +7,26 @@ import (
 )
 
 type Env struct {
-	port   string
-	apiKey string
-	dbUser string
-	dbPass string
-	dbName string
+	port      string
+	apiKey    string
+	dbUser    string
+	dbPass    string
+	dbName    string
+	appName   string
+	sigKeyJwt []byte
 }
+
+var env Env
 
 func LoadEnvironment() Env {
 	godotenv.Load()
-	environment := Env{port: os.Getenv("PORT"), apiKey: os.Getenv("API_KEY"), dbUser: os.Getenv("DB_USERNAME"), dbName: os.Getenv("DB_NAME"), dbPass: os.Getenv("DB_PASSWORD")}
+	environment := Env{
+		port:      os.Getenv("PORT"),
+		apiKey:    os.Getenv("API_KEY"),
+		dbUser:    os.Getenv("DB_USERNAME"),
+		dbName:    os.Getenv("DB_NAME"),
+		dbPass:    os.Getenv("DB_PASSWORD"),
+		appName:   os.Getenv("APPLICATION_NAME"),
+		sigKeyJwt: []byte(os.Getenv("JWT_SIGNATURE_KEY"))}
 	return environment
 }
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	// Load Environment
-	env := LoadEnvironment()
+	env = LoadEnvironment()
 
 	r := gin.Default()
 	r.SetTrustedProxies(nil)
