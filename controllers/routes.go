f61a2f4d52c7415340faea52d8a727a08960501b
davidf1000 <davidfauzi29@gmail.com>
2022-05-20 15:13:31 +0700

    finish autocomplete api implementation

--- controllers/routes.go
+++ controllers/routes.go
@@ -1,29 +1,58 @@
 package controllers
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"googlemaps.github.io/maps"
 )
+
+type Body struct {
+	Input string `json:"input"`
+}
+
+type Locations struct {
+	description string `json:"description"`
+}
+
+type ResponseAutoComplete struct {
+	predictions []Locations `json:"predictions"`
+}
 
 // GET /autocomplete
 // update user data with userid
 func AutocompleteLocation(c *gin.Context) {
 	fmt.Println("GET /autocomplete")
-	body, _ := ioutil.ReadAll(c.Request.Body)
-	fmt.Println("Data:", string(body))
-	var someString string = `{
-		"predictions" : [
-			{"text" : "Jalan sudirman"},
-			{"text" : "Jalan sutomo"},
-			{"text" : "Jalan sukajadi"}
-		]
-	}`
-	c.Data(http.StatusOK, "application/json; charset=utf-8", []byte(someString))
+	body := Body{}
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	client, err := maps.NewClient(maps.WithAPIKey(os.Getenv("API_KEY")))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	r := &maps.PlaceAutocompleteRequest{
+		Input:    body.Input,
+		Language: "id",
+	}
+	resp, err := client.PlaceAutocomplete(context.Background(), r)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"predictions": resp.Predictions})
 }
 
+type RoutesWithCarbonCalculated struct{
+	maps.Route,
+	carbon float32
+}
 // GET /routes
 // GET routes based on origin and destination
 func FindRoutes(c *gin.Context) {
