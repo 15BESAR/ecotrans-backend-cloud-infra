89142d88cd426c370f497d96468c4633749cc14f
davidf1000 <davidfauzi29@gmail.com>
2022-05-21 17:42:21 +0700

    implement get /users

--- controllers/users.go
+++ controllers/users.go
@@ -2,9 +2,9 @@ package controllers
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 
+	"github.com/15BESAR/ecotrans-backend-cloud-infra/models"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -13,28 +13,9 @@ import (
 // get all users data
 func FindUsers(c *gin.Context) {
 	fmt.Println("GET /users")
-	body, _ := ioutil.ReadAll(c.Request.Body)
-	fmt.Println("Data:", string(body))
-	var stringData string = `{
-		"users": [
-		  {
-			"firstName": "fname",
-			"lastName": "lname",
-			"ageOfBirth": "10-03-2000",
-			"age": 21,
-			"sex": "m/f",
-			"address": "loremipsum...",
-			"occupation": "student",
-			"point": 10202,
-			"totalRedeem": 100,
-			"totalDistance": 2023.5,
-			"totalEmissionReduced": 500,
-			"badge": 0
-		  }
-		]
-	  }`
-	// c.JSON(http.StatusOK, gin.H(stringData))
-	c.Data(http.StatusOK, "application/json; charset=utf-8", []byte(stringData))
+	var users []models.User
+	models.Db.Find(&users)
+	c.JSON(http.StatusOK, gin.H{"users": users})
 
 }
 
