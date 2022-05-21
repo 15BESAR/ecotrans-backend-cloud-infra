9d458f4e8ce058c44f08403dabb425407a50564e
davidf1000 <davidfauzi29@gmail.com>
2022-05-21 18:47:26 +0700

    implement POST /finish

--- controllers/journey.go
+++ controllers/journey.go
@@ -120,10 +120,11 @@ func AddJourney(c *gin.Context) {
 	}
 	// Check if user exist
 	var user models.User
-	if err := models.Db.Where("user_id = ?", c.Param("userId")).First(&user).Error; err != nil {
+	if err := models.Db.Where("user_id = ?", journey.UserID).First(&user).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "User not found!"})
 		return
 	}
+	// in the future maybe add checker to prevent double data
 	// add journey
 	models.Db.Create(&journey)
 	c.JSON(http.StatusOK, journey)
