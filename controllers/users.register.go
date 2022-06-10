5db6c9aaeda4b8980750e7e3eb4109653897a1f7
davidf1000 <davidfauzi29@gmail.com>
2022-06-10 18:13:12 +0700

    fix pass register

--- controllers/users.register.go
+++ controllers/users.register.go
@@ -21,16 +21,15 @@ func isValidEmail(email string) bool {
 func verifyPassword(s string) (sixOrMore, number, upper bool) {
 	letters := 0
 	for _, c := range s {
+		letters++
 		switch {
 		case unicode.IsNumber(c):
 			number = true
 		case unicode.IsUpper(c):
 			upper = true
-			letters++
 		// case unicode.IsPunct(c) || unicode.IsSymbol(c):
 		//     special = true
 		case unicode.IsLetter(c) || c == ' ':
-			letters++
 		default:
 			//return false, false, false, false
 		}
